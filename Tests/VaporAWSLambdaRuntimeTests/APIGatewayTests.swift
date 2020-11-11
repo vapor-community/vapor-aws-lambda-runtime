6662a0cb94461ef92e14cb5f7588720cbaf4aefe
Fabian Fett <fabianfett@mac.com>
2020-11-11 10:45:20 +0100

    Added tests for APIGateway and APIGatewayV2 (#14)

--- Tests/VaporAWSLambdaRuntimeTests/APIGatewayTests.swift
+++ Tests/VaporAWSLambdaRuntimeTests/APIGatewayTests.swift
@@ -1,8 +1,24 @@
-import AWSLambdaTesting
-import class Foundation.Bundle
-import VaporAWSLambdaRuntime
+import AWSLambdaEvents
+import Vapor
+@testable import VaporAWSLambdaRuntime
 import XCTest
 
 final class APIGatewayTests: XCTestCase {
-    func testAPIGatewayHandler() {}
+    func testCreateAPIGatewayResponse() {
+        let body = #"{"hello": "world"}"#
+        let vaporResponse = Vapor.Response(
+            status: .ok,
+            headers: HTTPHeaders([
+                ("Content-Type", "application/json"),
+            ]),
+            body: .init(string: body)
+        )
+
+        let response = APIGateway.Response(response: vaporResponse)
+
+        XCTAssertEqual(response.body, body)
+        XCTAssertEqual(response.multiValueHeaders?.count, 2)
+        XCTAssertEqual(response.multiValueHeaders?["Content-Type"]?.first, "application/json")
+        XCTAssertEqual(response.multiValueHeaders?["content-length"]?.first, String(body.count))
+    }
 }
